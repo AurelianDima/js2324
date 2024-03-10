1dc1cf33f12b314abeba3383231de5cf6be49eaf
AurelianDima <aurasvlogs@gmail.com>
2024-03-10 16:28:54 +0200

    js2324

---
+++ curs9/js/ap1.js
@@ -0,0 +1,7 @@
+let x = 10;
+if(x == 10){
+    console.log(x);
+}
+else{
+    console.log(11);
+}
---
+++ curs9/js/ap2.js
@@ -0,0 +1,6 @@
+try{
+    console.log('Prima alerta');
+} catch {
+    console.error('a 2 - a alerta');
+}
+console.log('Salut');
